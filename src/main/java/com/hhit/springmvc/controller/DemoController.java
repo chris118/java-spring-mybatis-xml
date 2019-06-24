4d6a4f08107fbc5c48fd670c84671b65a6aa9d6d
wangxiaopeng <wangxiaopeng@putao.com>
2019-06-24 17:46:16 +0800

    updage

--- src/main/java/com/hhit/springmvc/controller/DemoController.java
+++ src/main/java/com/hhit/springmvc/controller/DemoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 public class DemoController {
 
+    //-------------------Retrieve Single user--------------------------------------------------------
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> getUser(@PathVariable("id") long id) {
 
@@ -38,6 +39,18 @@ public class DemoController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    //-------------------Create a user--------------------------------------------------------
+    /*
+        http://localhost:8080/user/
+
+        body:
+        {
+          "username": "萨摩",
+          "password": "bbb",
+          "address": "黑龙江"
+        }
+     */
+
     @RequestMapping(value = "/user/", method = RequestMethod.POST)
     public ResponseEntity<Void> addUser(@RequestBody User user, UriComponentsBuilder ucBuilder) {
 
@@ -48,9 +61,6 @@ public class DemoController {
             userMapper.insertUser(user);
             sqlSession.commit();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setLocation(ucBuilder.path("/guest/{id}").buildAndExpand(user.getId()).toUri());
-            return new ResponseEntity<>(headers, HttpStatus.CREATED);
         } catch (Exception e) {
             e.printStackTrace();
             sqlSession.rollback();
@@ -60,5 +70,30 @@ public class DemoController {
             }
         }
         return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    //------------------- Delete a Guest --------------------------------------------------------
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") long id) {
+        System.out.println("Fetching & Deleting user with id " + id);
+
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = DBUtils.openSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            userMapper.deleteUser(id);
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            sqlSession.rollback();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
